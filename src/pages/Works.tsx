abc306e2709270580b71cd4eafbc650932ea9db8
viommuhaniya-ux <viommuhaniya@gmail.com>
2026-03-18 22:32:30 +0530

    Increase max width of Works component

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -251,7 +251,7 @@ export default function Works() {
               exit={{ scale: 0.95, opacity: 0 }}
               transition={{ duration: 0.3 }}
               onClick={(e) => e.stopPropagation()}
-              className="relative w-full max-w-2xl mx-auto px-4"
+              className="relative w-full max-w-5xl mx-auto px-4"
             >
               {/* Close button */}
               <button
